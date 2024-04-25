8d6c5faf12a631b58dc9895a696c88736dd119ef
smriti3002 <157886050+smriti3002@users.noreply.github.com>
2024-04-26 00:38:12 +0530

    complete sign up to UI

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,4 +1,5 @@
 // to create user defined errors like password is not long enough
+// i.e create a middleware and a function to handle possible errors
 
 export const errorHanlder = (statusCode , message)=>{
     const error = new Error()
