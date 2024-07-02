ab5e4d4adde30f72422a1fa9dc17a373966e54b4
smriti3002 <157886050+smriti3002@users.noreply.github.com>
2024-07-03 03:19:57 +0530

    add redux persist

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
